ae6e5122803df5f482da37e8090899d817788b82
reyanshkhaitan <77532864+reyanshkhaitan@users.noreply.github.com>
2021-08-27 17:01:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function setup() {
+    canvas = createCanvas(500, 500);
+    canvas.position(560, 150);
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose',getPoses);
+}
+
+function getPoses(results){
+    if(results.length > 0){
+        console.log(results);
+    }
+}
+
+function modelLoaded() { console.log("model is loaded"); }
+
+
+
+function draw() {
+    background('cyan');
+}
