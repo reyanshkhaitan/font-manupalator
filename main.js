6f7bf5ef9dc7d861b69a4c6624d63b85fd841d12
reyanshkhaitan <77532864+reyanshkhaitan@users.noreply.github.com>
2021-09-07 17:00:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
+var noseX = 0;
+var noseY = 0;
+var difference = 0;
+var rightWristX = 0;
+var leftWristX = 0;
+
 function setup() {
-    canvas = createCanvas(500, 500);
+    canvas = createCanvas(550, 500);
     canvas.position(560, 150);
     video = createCapture(VIDEO);
     video.size(550, 500);
@@ -11,6 +17,12 @@ function setup() {
 function getPoses(results){
     if(results.length > 0){
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX =" + noseX + "noseY = " + noseY);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX-rightWristX);
     }
 }
 
@@ -19,6 +31,11 @@ function modelLoaded() { console.log("model is loaded"); }
 
 
 function draw() {
-    background('cyan');
+    background('grey');
+    textSize(difference);
+    text('WIGGLE',noseX,300);
+   fill("maroon");
+    stroke("black");
+    //square(noseX,noseY,difference);
+    //document.getElementById("square_sides").innerHTML = "width and height of the square will be: " + difference + "px";
 }
-
